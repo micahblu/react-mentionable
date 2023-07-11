29f9347269c03e3ca420f0267e538fe341f88198
Micah Blu <micahblu@gmail.com>
2023-07-11 16:17:09 -0700

    Updates esbuild to include both cjs and esm modules

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,20 +2,34 @@ const { build } = require('esbuild')
 const InlineCSSPlugin = require('esbuild-plugin-inline-css')
 const { nodeExternalsPlugin } = require('esbuild-node-externals')
 const { Generator } = require('npm-dts')
+const { dependencies, peerDependencies } = require('./package.json')
 
 new Generator({
   entry: 'src/index.ts',
   output: 'dist/index.d.ts'
 }).generate()
 
-build({
+const sharedConfig = {
   entryPoints: ['src/index.ts'],
   bundle: true,
-  outfile: 'dist/index.js',
-	format: 'esm',
-	target: 'chrome79',
-	plugins: [
+  minify: true,
+  external: Object.keys(dependencies || {}).concat(Object.keys(peerDependencies || {})),
+  plugins: [
 		InlineCSSPlugin(),
 		nodeExternalsPlugin()
 	]
+}
+
+build({
+  ...sharedConfig,
+  platform: 'node', // for CJS
+  outfile: "dist/index.js"
 }).catch(() => process.exit(1))
+
+build({
+  ...sharedConfig,
+  outfile: "dist/index.esm.js",
+  platform: 'neutral', // for ESM
+  format: "esm"
+}).catch(() => process.exit(1))
+
