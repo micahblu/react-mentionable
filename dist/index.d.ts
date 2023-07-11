997f941f974c4e4ab5eae6317cfd1b081834a869
Micah Blu <micahblu@gmail.com>
2023-07-11 13:50:33 -0700

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,71 @@
 declare module 'react-mentionable/index' {
-  export function sum(num1: number, num2: number): number;
+  export { default } from 'react-mentionable/react-mentionable';
+
+}
+declare module 'react-mentionable/react-mentionable' {
+  import React from 'react';
+  import './styles.css';
+  export type Suggestion = {
+      label: string;
+      value: string;
+      [x: string]: any;
+  };
+  export type Mention = {
+      trigger: string;
+      highlightClassName?: string;
+      mentionClassname?: string;
+      suggestions: Array<Suggestion> | ((searchStr: string) => Promise<Array<Suggestion>>);
+  };
+  type ReactMenttionableProps = {
+      placeHolder?: string;
+      inputClass?: string;
+      suggestionsClass?: string;
+      defaultValue?: string;
+      onChange: (props: {
+          text: string;
+          markup: string;
+      }) => void;
+      mentions: Array<Mention>;
+      renderSuggestion?: (suggestion: Suggestion, selectSuggestion: (suggestion: Suggestion) => void) => React.ReactElement;
+      disabled?: boolean;
+  };
+  const ReactMentionable: React.ForwardRefExoticComponent<ReactMenttionableProps & React.RefAttributes<HTMLDivElement>>;
+  export default ReactMentionable;
+
+}
+declare module 'react-mentionable/stories/ReactMentionable.stories' {
+  import React from 'react';
+  import type { Meta, StoryObj } from '@storybook/react';
+  import './DemoStyles.css';
+  const Demo: () => React.JSX.Element;
+  const meta: Meta<typeof Demo>;
+  export default meta;
+  type Story = StoryObj<typeof Demo>;
+  export const Primary: Story;
+
+}
+declare module 'react-mentionable/utils' {
+  export const getLastKeyStroke: (el: HTMLDivElement | null) => string | undefined;
+  export const insertMention: ({ mentionClassname, editorEl, highlightEl, value, label, trigger }: {
+      mentionClassname: string;
+      editorEl: HTMLDivElement;
+      highlightEl: HTMLElement;
+      value: string;
+      label: string;
+      trigger: string;
+  }) => void;
+  export const removeHighlight: (editorEl: HTMLDivElement, highlightEl: HTMLElement) => void;
+  export const removeTrailingBreaks: (el: HTMLElement | Node) => void;
+  export const removeFontTags: (el: HTMLElement | null) => void;
+  export const getLastElement: (parentEl: HTMLElement, refNode?: HTMLElement) => HTMLElement | undefined;
+  export const getLastNode: (parentNode: HTMLElement, refNode?: Node) => ChildNode | undefined;
+  export const getCaretPosition: (element: HTMLElement) => number;
+  export const insertAtCaretPos: (parentEl: HTMLElement, insertEl: HTMLElement | ChildNode) => void;
+  export const insertAfter: (newNode: Node, existingNode: Node) => void;
+  export const autoPositionCaret: (anchorNode: Node) => void;
+  export const scrollIntoView: (editorEl: HTMLElement) => void;
+  export const debounce: (callback: Function, interval: number) => Function;
+  export const convertToMarkup: (html: string) => string;
 
 }
 declare module 'react-mentionable' {
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,389 @@
-// src/index.ts
-function sum(num1, num2) {
-  return num1 + num2;
+// src/react-mentionable.tsx
+import React, { useRef, useState, useEffect, useLayoutEffect, forwardRef } from "react";
+
+// src/utils.ts
+var getLastKeyStroke = (el) => {
+  if (!el)
+    return;
+  const caretPos = getCaretPosition(el);
+  return el.innerText.at(caretPos - 1);
+};
+var insertMention = ({
+  mentionClassname,
+  editorEl,
+  highlightEl,
+  value,
+  label,
+  trigger
+}) => {
+  var _a;
+  const mentionEl = document.createElement("span");
+  mentionEl.className = mentionClassname;
+  mentionEl.setAttribute("trigger", trigger);
+  mentionEl.setAttribute("value", value);
+  mentionEl.contentEditable = "false";
+  mentionEl.innerText = label;
+  insertAfter(mentionEl, highlightEl);
+  const anchorTextNode = document.createTextNode("\xA0");
+  insertAfter(anchorTextNode, mentionEl);
+  editorEl.removeChild(highlightEl);
+  (_a = highlightEl.parentNode) == null ? void 0 : _a.removeChild(highlightEl);
+  scrollIntoView(editorEl);
+  autoPositionCaret(editorEl);
+};
+var removeHighlight = (editorEl, highlightEl) => {
+  const textNode = document.createTextNode(highlightEl.innerText);
+  insertAfter(textNode, highlightEl);
+  if (highlightEl.parentNode) {
+    highlightEl.parentNode.removeChild(highlightEl);
+  } else {
+    editorEl.removeChild(highlightEl);
+  }
+};
+var removeTrailingBreaks = (el) => {
+  var _a;
+  const nodes = el.childNodes;
+  const len = nodes.length;
+  for (let i = len; i > 0; i--) {
+    const node = nodes[i];
+    if ((node == null ? void 0 : node.nodeName) === "#text")
+      return;
+    if ((node == null ? void 0 : node.nodeName) === "BR") {
+      (_a = node.parentNode) == null ? void 0 : _a.removeChild(node);
+    }
+  }
+};
+var removeFontTags = (el) => {
+  var _a, _b;
+  if (!el)
+    return;
+  const nodes = el.getElementsByTagName("font");
+  if (!nodes.length)
+    return;
+  for (let i = 0; i < nodes.length; i++) {
+    const text = nodes[i].innerText;
+    const textNode = document.createTextNode(text);
+    (_a = nodes[i].parentNode) == null ? void 0 : _a.appendChild(textNode);
+    (_b = nodes[i].parentNode) == null ? void 0 : _b.removeChild(nodes[i]);
+  }
+  autoPositionCaret(el);
+};
+var getLastElement = (parentEl, refNode) => {
+  const len = parentEl.childNodes.length - 1;
+  let lastElement;
+  for (let i = 0; i <= len; i++) {
+    const currentEl = parentEl.childNodes[i];
+    if (currentEl.nodeName !== "#text") {
+      lastElement = currentEl;
+    }
+    if (currentEl === refNode)
+      break;
+  }
+  return lastElement;
+};
+var getLastNode = (parentNode, refNode) => {
+  if (!refNode) {
+    return parentNode.childNodes[parentNode.childNodes.length - 1];
+  }
+  const len = parentNode.childNodes.length - 1;
+  let lastNode;
+  for (let i = 0; i <= len; i++) {
+    lastNode = parentNode.childNodes[i];
+    if (lastNode === refNode)
+      break;
+  }
+  return lastNode;
+};
+var getCaretPosition = (element) => {
+  var _a;
+  let caretOffset = 0;
+  if (window.getSelection) {
+    var range = (_a = window.getSelection()) == null ? void 0 : _a.getRangeAt(0);
+    if (!range)
+      return caretOffset;
+    var preCaretRange = range.cloneRange();
+    preCaretRange.selectNodeContents(element);
+    preCaretRange.setEnd(range.endContainer, range.endOffset);
+    caretOffset = preCaretRange.toString().length;
+  }
+  return caretOffset;
+};
+var insertAtCaretPos = (parentEl, insertEl) => {
+  var _a, _b, _c, _d;
+  const selection = window.getSelection();
+  const anchorNode = selection == null ? void 0 : selection.anchorNode;
+  if (!anchorNode)
+    return;
+  const caretPos = getCaretPosition(parentEl);
+  let charCount = 0;
+  for (let i = 0; i < parentEl.childNodes.length; i++) {
+    const child = parentEl.childNodes[i];
+    if (child === anchorNode)
+      break;
+    if (child.innerText) {
+      charCount += child.innerText.length;
+    } else {
+      charCount += ((_a = child.nodeValue) == null ? void 0 : _a.length) || 0;
+    }
+  }
+  if (anchorNode === parentEl || charCount === caretPos) {
+    if (caretPos === 0 && parentEl.firstChild) {
+      parentEl.insertBefore(insertEl, parentEl.firstChild);
+    } else {
+      parentEl.appendChild(insertEl);
+    }
+    return;
+  }
+  let anchorCaretPos;
+  if (parentEl.firstChild === anchorNode) {
+    anchorCaretPos = caretPos;
+  } else {
+    anchorCaretPos = caretPos - charCount;
+  }
+  const beforeNode = document.createTextNode(((_b = anchorNode.nodeValue) == null ? void 0 : _b.substring(0, anchorCaretPos)) || "");
+  const afterNode = document.createTextNode(((_c = anchorNode.nodeValue) == null ? void 0 : _c.substring(anchorCaretPos)) || "");
+  const nextSibling = anchorNode.nextSibling;
+  (_d = anchorNode.parentNode) == null ? void 0 : _d.removeChild(anchorNode);
+  if (nextSibling) {
+    parentEl.insertBefore(afterNode, nextSibling);
+    parentEl.insertBefore(insertEl, afterNode);
+    parentEl.insertBefore(beforeNode, insertEl);
+  } else {
+    parentEl.appendChild(beforeNode);
+    parentEl.appendChild(insertEl);
+    parentEl.appendChild(afterNode);
+  }
+};
+var insertAfter = (newNode, existingNode) => {
+  var _a;
+  (_a = existingNode.parentNode) == null ? void 0 : _a.insertBefore(newNode, existingNode.nextSibling);
+};
+var autoPositionCaret = (anchorNode) => {
+  const selection = window.getSelection();
+  const anchor = anchorNode ? anchorNode : selection == null ? void 0 : selection.anchorNode;
+  if (!anchor)
+    return;
+  selection == null ? void 0 : selection.collapse(anchor, anchor.childNodes.length);
+};
+var scrollIntoView = (editorEl) => {
+  const lastElement = getLastElement(editorEl);
+  if (lastElement == null ? void 0 : lastElement.scrollIntoView) {
+    lastElement.scrollIntoView();
+  }
+};
+var convertToMarkup = (html) => {
+  const mentionRegex = /(<[^>]+>)([^<]+)<\/[^>]+>/g;
+  const convertedMarkup = html.replace(/&nbsp;/g, " ").replace(mentionRegex, (match, p1, p2) => {
+    const triggerRegex = /trigger="(.)"/;
+    const valueRegex = /value="([^"]+)"/;
+    const triggerMatch = p1.match(triggerRegex);
+    const valueMatch = p1.match(valueRegex);
+    if (!triggerMatch || !valueMatch) {
+      return p2;
+    }
+    const trigger = triggerMatch[1];
+    const value = valueMatch[1];
+    return `__${trigger}[${p2}](${value})__ `;
+  });
+  return convertedMarkup.replace(/<[^>]+>/g, " ");
+};
+
+// src/styles.css
+(function() {
+  if (!document.getElementById("2d76d2ce1c48b868ea7bbaf5c310ae464f67278ef49d495dbb8ac1501a5660bd")) {
+    var e = document.createElement("style");
+    e.id = "2d76d2ce1c48b868ea7bbaf5c310ae464f67278ef49d495dbb8ac1501a5660bd";
+    e.textContent = `.react-mentionable-suggestions {
+  position: relative;
+  padding: 1rem;
+  border: 1px solid #ccc;
 }
+
+.react-mentionable-input {
+  padding: 1rem;
+  border: 1px solid #ccc;
+}
+`;
+    document.head.appendChild(e);
+  }
+})();
+
+// src/react-mentionable.tsx
+var ReactMentionable = forwardRef((props, ref) => {
+  const {
+    placeHolder,
+    inputClass,
+    suggestionsClass,
+    mentions,
+    onChange,
+    renderSuggestion,
+    disabled
+  } = props;
+  const editorRef = useRef();
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
+  const triggers = mentions.map((mention) => mention.trigger);
+  let isMatching = useRef(false);
+  let currentTrigger = useRef();
+  let matches = useRef([]);
+  let highlightEl = useRef();
+  const selectSuggestion = (suggestion) => {
+    var _a;
+    if (!editorRef.current || !highlightEl.current)
+      return;
+    insertMention({
+      mentionClassname: ((_a = mentions.find((m) => m.trigger === currentTrigger.current)) == null ? void 0 : _a.mentionClassname) || "",
+      trigger: currentTrigger.current || "",
+      value: suggestion.value,
+      editorEl: editorRef.current,
+      label: suggestion.label,
+      highlightEl: highlightEl.current
+    });
+    setShowSuggestions(false);
+    isMatching.current = false;
+  };
+  useEffect(() => {
+    if (disabled && editorRef.current) {
+      editorRef.current.setAttribute("contenteditable", "false");
+      editorRef.current.style.opacity = "0.5";
+    }
+  }, [disabled]);
+  useLayoutEffect(() => {
+    if (!(editorRef == null ? void 0 : editorRef.current))
+      return;
+    const keyUpListener = (e) => {
+      var _a, _b, _c, _d, _e, _f;
+      if (!editorRef.current)
+        return;
+      removeFontTags(editorRef.current);
+      const key = e.key || getLastKeyStroke(editorRef.current);
+      if (isMatching.current && key === "Tab" || key === " ") {
+        const lastNode = getLastNode(editorRef.current);
+        if (!lastNode)
+          return;
+        const nodeText = ((_a = lastNode == null ? void 0 : lastNode.nodeValue) == null ? void 0 : _a.replace(currentTrigger.current || "", "").toLowerCase()) || "";
+        if (highlightEl.current && (matches.current.length === 1 && isMatching.current) || matches.current.map((m) => m.label).includes(nodeText)) {
+          insertMention({
+            mentionClassname: ((_b = mentions.find((m) => m.trigger === currentTrigger.current)) == null ? void 0 : _b.mentionClassname) || "",
+            trigger: currentTrigger.current || "",
+            value: matches.current[0].value,
+            editorEl: editorRef.current,
+            label: matches.current[0].label,
+            highlightEl: highlightEl.current
+          });
+        } else if (isMatching.current && matches.current.length !== 1) {
+          removeHighlight(editorRef.current, highlightEl.current);
+          autoPositionCaret(editorRef.current);
+        }
+        isMatching.current = false;
+        setShowSuggestions(false);
+      } else if (key === "Backspace") {
+        e.preventDefault();
+        const selection = window.getSelection();
+        const anchorNode = selection == null ? void 0 : selection.anchorNode;
+        if (!anchorNode)
+          return;
+        const last = anchorNode.childNodes.length || 0;
+        const lastAnchorChild = anchorNode.childNodes[last - 1];
+        if ((lastAnchorChild == null ? void 0 : lastAnchorChild.nodeValue) === "") {
+          (_c = lastAnchorChild == null ? void 0 : lastAnchorChild.parentNode) == null ? void 0 : _c.removeChild(lastAnchorChild);
+        }
+        removeTrailingBreaks(anchorNode);
+        removeTrailingBreaks(editorRef.current);
+        if (((_d = highlightEl.current) == null ? void 0 : _d.innerText.length) === 1) {
+          e.preventDefault();
+          setShowSuggestions(false);
+          isMatching.current = false;
+        }
+      } else if (isMatching.current && key !== currentTrigger.current) {
+        const inputStr = ((_e = highlightEl.current) == null ? void 0 : _e.innerText) || "";
+        const symbolIndex = inputStr.lastIndexOf(currentTrigger.current || "");
+        const searchStr = inputStr.substr(symbolIndex + 1).replace(/[^\w]/, "");
+        const regex = new RegExp(searchStr, "i");
+        const mention = mentions.find((m) => m.trigger === currentTrigger.current);
+        const suggestions2 = (_f = mentions.find((m) => m.trigger === currentTrigger.current)) == null ? void 0 : _f.suggestions;
+        if (Array.isArray(mention == null ? void 0 : mention.suggestions)) {
+          if (suggestions2) {
+            matches.current = suggestions2.filter((suggestion) => regex.test(suggestion.label));
+            setSuggestions(matches.current);
+          }
+        } else {
+          mention == null ? void 0 : mention.suggestions(searchStr).then((suggested) => {
+            matches.current = suggested.filter((suggestion) => regex.test(suggestion.label));
+            setSuggestions(matches.current);
+          });
+        }
+      }
+      onChange({
+        text: editorRef.current.innerText,
+        markup: convertToMarkup(editorRef.current.innerHTML)
+      });
+    };
+    const keyDownListener = (e) => {
+      var _a;
+      const key = e.key || getLastKeyStroke(editorRef.current);
+      if (!key || !editorRef.current)
+        return;
+      if (key === "Enter") {
+        e.preventDefault();
+        const br = document.createElement("br");
+        const textNode = document.createTextNode("\u200B");
+        insertAtCaretPos(editorRef.current, br);
+        insertAfter(textNode, br);
+        autoPositionCaret(editorRef.current);
+      } else if (key === "Tab")
+        e.preventDefault();
+      else if (triggers.includes(key)) {
+        if (isMatching.current) {
+          e.preventDefault();
+          return;
+        }
+        currentTrigger.current = key;
+        isMatching.current = true;
+        highlightEl.current = document.createElement("span");
+        highlightEl.current.className = `${(_a = mentions.find((m) => m.trigger === currentTrigger.current)) == null ? void 0 : _a.highlightClassName}`;
+        highlightEl.current.innerText = currentTrigger.current;
+        highlightEl.current.setAttribute("contentEditable", "true");
+        insertAtCaretPos(editorRef.current, highlightEl.current);
+        setShowSuggestions(true);
+        autoPositionCaret(highlightEl.current);
+        scrollIntoView(editorRef.current);
+        e.preventDefault();
+      }
+    };
+    editorRef.current.addEventListener("keydown", keyDownListener);
+    editorRef.current.addEventListener("keyup", keyUpListener);
+  }, []);
+  return /* @__PURE__ */ React.createElement("div", { className: "react-mentionable" }, /* @__PURE__ */ React.createElement("div", { className: "react-mentionable-editor-container" }, /* @__PURE__ */ React.createElement(
+    "div",
+    {
+      placeholder: placeHolder,
+      className: `react-mentionable-input ${inputClass}`,
+      ref: (node) => {
+        editorRef.current = node;
+        if (typeof ref === "function") {
+          ref(node);
+        } else if (ref) {
+          ref.current;
+        }
+      },
+      contentEditable: true
+    }
+  )), showSuggestions && /* @__PURE__ */ React.createElement("div", { className: `react-mentionable-suggestions ${suggestionsClass}` }, suggestions && suggestions.map((suggestion) => {
+    if (renderSuggestion)
+      return renderSuggestion(suggestion, selectSuggestion);
+    return /* @__PURE__ */ React.createElement(
+      "div",
+      {
+        onClick: () => selectSuggestion(suggestion),
+        key: suggestion.label,
+        className: "react-mentionable-suggestion"
+      },
+      suggestion.label
+    );
+  })));
+});
+var react_mentionable_default = ReactMentionable;
 export {
-  sum
+  react_mentionable_default as default
 };
